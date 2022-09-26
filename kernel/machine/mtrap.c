0139e833ee1758bc51997308432083fcd30fa22d
Merky <merky@hust.edu.cn>
2022-09-26 11:26:22 +0800

    finish 1-2

--- kernel/machine/mtrap.c
+++ kernel/machine/mtrap.c
@@ -31,7 +31,8 @@ void handle_mtrap() {
     case CAUSE_ILLEGAL_INSTRUCTION:
       // TODO (lab1_2): call handle_illegal_instruction to implement illegal instruction
       // interception, and finish lab1_2.
-      panic( "call handle_illegal_instruction to accomplish illegal instruction interception for lab1_2.\n" );
+      handle_illegal_instruction();
+      //panic( "call handle_illegal_instruction to accomplish illegal instruction interception for lab1_2.\n" );
 
       break;
     case CAUSE_MISALIGNED_LOAD:
