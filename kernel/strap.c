5f29f0c257703c0e6b89bce7d72ab5a70bc50efd
Caiji Zhou <cjz210oliver@gmail.com>
2022-12-15 15:10:54 +0800

    lab3_3

--- kernel/strap.c
+++ kernel/strap.c
@@ -36,7 +36,8 @@ void handle_mtimer_trap() {
   // TODO (lab1_3): increase g_ticks to record this "tick", and then clear the "SIP"
   // field in sip register.
   // hint: use write_csr to disable the SIP_SSIP bit in sip.
-  panic( "lab1_3: increase g_ticks by one, and clear SIP field in sip register.\n" );
+  g_ticks += 1;
+  write_csr(sip, 0);
 
 }
 
